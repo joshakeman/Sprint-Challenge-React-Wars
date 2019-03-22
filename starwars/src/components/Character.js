eedcf4bea330bff6978860c577f55e506af1692d
Josh Akeman <joshakeman@gmail.com>
2019-03-22 12:51:17 -0400

    Got characters displaying

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -4,7 +4,15 @@ const Character = (props) => {
     // console.log('Your Props are ' + props.params)
     return (
     <div class="character-card">
-        <h3>Name: {props.name}</h3>
+        <h3>Name: {props.charInfo.name}</h3>
+        <ul className="list">
+            <li className="list-item"><strong>Born: </strong> {props.charInfo.birth_year}</li>
+            <li className="list-item"><strong>Eye Color: </strong> {props.charInfo.eye_color}</li>
+            <li className="list-item"><strong>Height: </strong> {props.charInfo.height}</li>
+            <li className="list-item"><strong>Mass: </strong> {props.charInfo.mass}</li>
+            <li className="list-item"><strong>Hair Color: </strong> {props.charInfo.hair_color}</li>
+        </ul>
+
     </div>
     )
 }
